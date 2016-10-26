8b5a6de098cdd53a45951eea277b69812046579e
Ahsanuzzaman Khan <ahsan.khan434@gmail.com>
2016-10-26 18:35:29 +0600

    Bookmark model added.

---
+++ app/models/bookmark.js
@@ -0,0 +1,17 @@
+var mongoose  = require('mongoose');
+var Schema    = mongoose.Schema;
+
+var BookmarkSchema = new Schema({
+    title: String,
+    imageUrl: String,
+    url: String,
+    description: String,
+    users: [
+        {type: Schema.Types.ObjectId, ref: 'User'}
+    ],
+    isPrivate: Boolean,
+    created: Date,
+    updated: Date
+});
+
+module.exports = mongoose.model('Bookmark', BookmarkSchema);
